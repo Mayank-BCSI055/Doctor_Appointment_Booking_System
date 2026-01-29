725e6a2340f7f3d4c8c5891f05efecc6b85a30a7
Mayank-BCSI055 <mayank.baranwal@walsystems.in>
2026-01-29 12:57:06 +0530

    Fix Render port binding

--- doctor-appointment-backend/run.py
+++ doctor-appointment-backend/run.py
@@ -1,5 +1,7 @@
+
 from app.__init__ import create_app
 from app.database.seed_admin import seed_admin
+import os
 
 app = create_app()
 
@@ -7,4 +9,6 @@ with app.app_context():
     seed_admin()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
